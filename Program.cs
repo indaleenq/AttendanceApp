33497823c51ee95b57c2af3bd9ac9a9fdd8177e8
indaleen <inquinsayas@gmail.com>
2022-11-19 11:53:42 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AttendanceApp //package
+{
+    internal class Program //class
+    {
+        static void Main() //main
+        {
+            int x = Convert.ToInt32(Console.ReadLine()); //1
+            int y = Convert.ToInt32(Console.ReadLine()); //2
+
+            Console.WriteLine($"{x} + {y} = {x + y}");
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+            //output -- 1 + 2 = 3
+            Console.WriteLine("{2} + {0} = {1}", x, y, x + y);
+            //output -- 3 + 1 = 2
+
+            Console.WriteLine("{0} - {1} = {2}", x, y, x - y);
+            Console.WriteLine("{0} x {1} = {2}", x, y, x * y);
+            Console.WriteLine("{0} / {1} = {2}", x, y, x / y);
+            Console.WriteLine("{0} mod {1} = {2}", x, y, x % y);
+        }
+    }
+}
